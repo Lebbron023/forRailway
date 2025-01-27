c773401da635f2291d3b578a47dc0dbff1bad411
Lebbron <lebbron023@gmail.com>
2025-01-27 22:23:00 +0800

    change server listen port

--- backend/server.js
+++ backend/server.js
@@ -167,6 +167,6 @@ app.get('/accounts', (req, res) => {
 
 
 // Start the server
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
   });
