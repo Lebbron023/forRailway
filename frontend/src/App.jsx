a33397d6c8dc62cad7e8bd82cac6749c33c577bb
Lebbron <lebbron023@gmail.com>
2025-01-28 13:18:10 +0800

    1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,15 +7,15 @@ import Register from './auth/Register';
 
 import React from 'react';
 
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/register" />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
